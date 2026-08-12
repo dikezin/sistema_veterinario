8957929e4ff8b1a07202086ae7b9e64052c4167a
Andres Dike <dikegod12@gmail.com>
2026-08-12 00:25:55 -0500

    Organize project into front back and files layers

---
+++ src/main/java/com/mycompany/sistema_veterinario/back/model/Productos.java
@@ -0,0 +1,53 @@
+package com.mycompany.sistema_veterinario.back.model;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author dikeg
+ */
+public class Productos {
+  private String id;
+    private String nombre;
+    private String categoria;
+    private int stock;
+    private double precio;
+
+    public Productos(String id, String nombre, String categoria, int stock, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.stock = stock;
+        this.precio = precio;
+    }
+
+    // GETTERS
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    // SETTERS
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
---
+++ src/main/java/com/mycompany/sistema_veterinario/back/model/Usuarios.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistema_veterinario.back.model;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author home
+ */
+public class Usuarios {
+    String nombre;
+    String apellido;
+    String cedula;
+    String correo; 
+    String pass;
+    String rol;
+    public Usuarios(String cedula, String nombre, String apellido, String correo,
+            String pass, String rol){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.cedula=cedula;
+        this.correo=correo;
+        this.pass=pass;
+        this.rol=rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+public String getCedula(){
+    return cedula;
+}
+    public String getApellido() {
+        return apellido;
+    }
+
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+    public String getRol(){
+        return rol;
+    }
+     public boolean existe() {
+        File archivo = new File("usuarios/" + cedula + ".json");
+        return archivo.exists();
+    }
+
+    // Guardar usuario en JSON
+    /*public void guardar() throws IOException {
+        File carpeta = new File("usuarios");
+        if (!carpeta.exists()) {
+            carpeta.mkdirs();
+        }
+
+        File archivo = new File(carpeta, cedula + ".json");
+
+        FileWriter fw = new FileWriter(archivo);
+        fw.write(
+            "{\n" +
+            "  \"cedula\": \"" + cedula + "\",\n" +
+            "  \"nombre\": \"" + nombre + "\",\n" +
+            "  \"correo\": \"" + correo + "\",\n" +
+            "  \"password\": \"" + pass + "\"\n" +
+            "}"
+        );
+        fw.close();
+    }*/
+
+            
+            
+}
